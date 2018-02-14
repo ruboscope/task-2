8317864528c069fd1a9820859b56f26ce9651c65
Zhukov Andrey <AS.Zhukow@BelVeb.by>
2018-02-14 09:17:42 +0300

    - проинициализированы основные переменные

--- js/solution.js
+++ js/solution.js
@@ -11,6 +11,24 @@
      */
     function solution(map) {
         // todo: подсчитать кол-во островов на карте
+        
+        var leftField, //соседнее левое поле на карте
+            upField, // соседнее правое поле на карте
+            numOfIslands; // кол-во островов
+
+        /* Массив в котором будем искать и считать количество островов. 
+        Изначально он заполнен нулями. Размерность как у map*/
+        var countingMap = new Array(map.length);
+        for (var i = 0; i < countingMap.length; i++) {
+            countingMap[i] = new Array(map[i].length);
+            for (var j = 0; j < countingMap[i].length; j++) {
+                countingMap[i][j] = 0;
+            }
+        }
+
+        var groupList = [0]; // Массив, в котором будет считаться количество островов(к какому острову относиться поле суши)
+
+        var groupNum = 1; // Номер группы острова
         return 0;
     }
 
